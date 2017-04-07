a7a9d49f8a14c10c834f2c18ac94bb74f5ed0505
chenli <chenli@192.168.43.24>
2017-04-07 11:43:51 +0800

    new demos

--- Swagger/src/main/java/evolution/util/Ref.java
+++ Swagger/src/main/java/evolution/util/Ref.java
@@ -2,33 +2,26 @@ package evolution.util;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.Date;
+import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 public class Ref {
-	public static Boolean isList(Object object) {
-		return isList(object.getClass());
+	public static Boolean isMap(Class<?> clazz) {
+		return clazz == Map.class;
 	}
-
-	public static Boolean isList(Field field) {
-		return isList(field.getType());
+	
+	public static Boolean isMap(Object object) {
+		return isMap(object.getClass());
 	}
-
-	public static Boolean isList(Class<?> clazz) {
-		if (clazz == List.class) {
-			return true;
-		}
-		return false;
+	
+	public static Boolean isMap(Field field) {
+		return isMap(field.getType());
 	}
-
-	public static Boolean isBasic(Object object) {
-		return isBasic(object.getClass());
-	}
-
-	public static Boolean isBasic(Field field) {
-		return isBasic(field.getType());
-	}
+	
 	// Get the annotation of a class, method or field object.
 	@SuppressWarnings("unchecked")
 	public static <T> T annotation(Object object, Class<T> annotationClass) {
@@ -39,14 +32,6 @@ public class Ref {
 			e.printStackTrace();
 			return null;
 		}
-	}
-
-	public static Object defaultBasicObject(Field field) {
-		return defaultBasicObject(field.getType());
-	}
-
-	public static Object defaultBasicObject(Object object) {
-		return defaultBasicObject(object.getClass());
 	}
 
 	public static Object defaultBasicObject(Class<?> clazz) {
@@ -63,18 +48,12 @@ public class Ref {
 		return null;
 	}
 
-	public static Class<?> genericClass(Field field, int i) {
-		ParameterizedType parameterizedType = (ParameterizedType) field.getGenericType();
-		return (Class<?>) parameterizedType.getActualTypeArguments()[i];
+	public static Object defaultBasicObject(Field field) {
+		return defaultBasicObject(field.getType());
 	}
 
-	public static Object defaultObject(Field field) {
-		try {
-			return defaultObject(field.getType().newInstance());
-		} catch (InstantiationException | IllegalAccessException e) {
-			e.printStackTrace();
-			return null;
-		}
+	public static Object defaultBasicObject(Object object) {
+		return defaultBasicObject(object.getClass());
 	}
 
 	public static Object defaultObject(Class<?> clazz) {
@@ -85,32 +64,65 @@ public class Ref {
 			return null;
 		}
 	}
+	
+	public static Object defaultObject(Field field) {
+		try {
+			return defaultObject(field.getType().newInstance());
+		} catch (InstantiationException | IllegalAccessException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public static Object defaultObject(Object object) {
 		try {
-			if (Ref.isBasic(object)) {
+			if (isBasic(object)) {
 				return defaultBasicObject(object);
 			}
 			Field[] fields = object.getClass().getDeclaredFields();
 			for (Field field : fields) {
 				field.setAccessible(true);
-				if (Ref.isBasic(field)) {
+				if (isBasic(field)) {
 					field.set(object, defaultBasicObject(field));
-				} else if (Ref.isList(field)) {
+				} else if (isList(field)) {
 					List list = new LinkedList();
 					list.add(defaultObject(genericClass(field, 0)));
 					field.set(object, list);
+				} else if (isMap(field)) {
+					Map map = new LinkedHashMap();
+					List<Class<?>> genericClasses = genericClasses(field);
+					map.put(defaultObject(genericClasses.get(0)), defaultObject(genericClasses.get(1)));
+					field.set(object, map);
 				} else {
 					field.set(object, defaultObject(field));
 				}
 			}
 			return object;
 		} catch (Exception e) {
-			Sys.println("The invalid object is " + object);
-			return object;
+			Sys.println(object + " is invalid.");
+			return null;
 		}
 	}
+
+	public static Type[] actualTypeArguments(Field field) {
+		ParameterizedType parameterizedType = (ParameterizedType) field.getGenericType();
+		return parameterizedType.getActualTypeArguments();
+	}
+	
+	public static List<Class<?>> genericClasses(Field field) {
+		List<Class<?>> genericClasses = new LinkedList<>();
+		Type[] types = actualTypeArguments(field);
+		for (Type type : types) {
+			genericClasses.add((Class<?>) type);
+		}
+		return genericClasses;
+	}
+	
+	public static Class<?> genericClass(Field field, int i) {
+		return (Class<?>) actualTypeArguments(field)[i];
+	}
+
 	public static Boolean isBasic(Class<?> clazz) {
 		// TODO Add more criteria.
 		if (clazz == int.class || clazz == Integer.class
@@ -120,5 +132,25 @@ public class Ref {
 			return true;
 		}
 		return false;
+	}
+
+	public static Boolean isBasic(Field field) {
+		return isBasic(field.getType());
+	}
+
+	public static Boolean isBasic(Object object) {
+		return isBasic(object.getClass());
+	}
+
+	public static Boolean isList(Class<?> clazz) {
+		return clazz == List.class;
+	}
+
+	public static Boolean isList(Field field) {
+		return isList(field.getType());
+	}
+	
+	public static Boolean isList(Object object) {
+		return isList(object.getClass());
 	}
 }
